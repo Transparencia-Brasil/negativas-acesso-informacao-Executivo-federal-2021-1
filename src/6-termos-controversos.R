a851ad17cd0b90a822de6f0992d41cfe4901afe3
rdurl0 <durlo.raul@gmail.com>
2021-09-22 21:00:09 -0300

    edita script

--- src/6-termos-controversos.R
+++ src/6-termos-controversos.R
@@ -8,20 +8,23 @@ recursos <- readRDS(here("dados/load/rds/recursos_clean.rds"))
 
 # termos controversos ----------------------------------------------------------
 
+regex_lgpd <- 'lei geral de proteção de dados|lei de protecao de dados pessoais|13\\.?709|lgpd'
+
 controversos <- pedidos %>% 
   rename(resposta_pedido = resposta) %>% 
-  mutate(
-    controversos_desarrazoado = str_detect(resposta_pedido,'desarrazoado|desproporciona'), 
-    controversos_fishing  = str_detect(resposta_pedido,'fishing'),
-    controversos_seguranca =  str_detect(resposta_pedido,'segurança [nacional|do estado]'),
-    controversos_sigilo = str_detect(resposta_pedido,'sigilo'),
-    controversos_decisao =  str_detect(resposta_pedido,'processo decisorio em curso|documento preparatorio'),
-    controversos_trabalho_adic = str_detect(resposta_pedido,'trabalho[s]? adiciona'),
-    controversos_generico =  str_detect(resposta_pedido,'generico'),
-    controversos_lgpd = str_detect(resposta_pedido, 'lei geral de proteção de dados|lei de protecao de dados pessoais|13\\.?709|13\\.?853|lgpd')
-  )
+  mutate(controversos_lgpd = str_detect(resposta_pedido, regex_lgpd))
 
-pedidos %>% 
-  select(id_pedido, starts_with("controversos")) %>% 
-  pivot_longer(names_to = "controversos", cols = starts_with("controversos"), 
-               values_to = "prevalencia", names_prefix = "controversos_")
+controversos_lgpd <- controversos %>% 
+  select(id_pedido, data_resposta, orgao = orgaodestinatario, controversos_lgpd) %>% 
+  filter(controversos_lgpd) %>% 
+  mutate(controverso_id = "LGPD")
+
+saveRDS(controversos_lgpd, here("dados/load/rds/controversos_lgpd.rds"))
+
+# controversos_desarrazoado = str_detect(resposta_pedido,'desarrazoado|desproporciona'), 
+# controversos_fishing  = str_detect(resposta_pedido,'fishing'),
+# controversos_seguranca =  str_detect(resposta_pedido,'segurança [nacional|do estado]'),
+# controversos_sigilo = str_detect(resposta_pedido,'sigilo'),
+# controversos_decisao =  str_detect(resposta_pedido,'processo decisorio em curso|documento preparatorio'),
+# controversos_trabalho_adic = str_detect(resposta_pedido,'trabalho[s]? adiciona'),
+# controversos_generico =  str_detect(resposta_pedido,'generico'),
