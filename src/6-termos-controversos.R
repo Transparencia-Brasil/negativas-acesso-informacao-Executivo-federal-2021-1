54d4d668261f47fda60501dbe5831aea7cf7b25d
rdurl0 <durlo.raul@gmail.com>
2021-10-27 17:17:35 -0300

    melhoria na NLP

--- src/6-termos-controversos.R
+++ src/6-termos-controversos.R
@@ -10,16 +10,49 @@ recursos <- readRDS(here("dados/load/rds/recursos-clean.rds"))
 
 regex_lgpd <- 'lei geral de proteção de dados|lei de protecao de dados pessoais|13\\.?709|lgpd'
 
-controversos <- pedidos %>% 
+pedidos_controversos_lgpd <- pedidos %>% 
   rename(resposta_pedido = resposta) %>% 
   mutate(controversos_lgpd_resposta = str_detect(resposta_pedido, regex_lgpd),
-         controversos_lgpd_pedido = str_detect(detalhamento_solicitacao, regex_lgpd))
-
-controversos_lgpd <- controversos %>% 
+         controversos_lgpd_pedido = str_detect(detalhamento_solicitacao, regex_lgpd)
+         ) %>% 
   select(id_pedido, data_resposta, orgao = orgaodestinatario, contains("controversos_lgpd")) %>% 
-  mutate(controverso_id = "LGPD")
+  mutate(
+    controverso_id = "LGPD",
+    tipo = case_when(
+      controversos_lgpd_pedido & controversos_lgpd_resposta ~ "Pedido e reposta mencionam LGPD",
+      controversos_lgpd_pedido & !controversos_lgpd_resposta ~ "Pedido menciona LGPD, resposta não",
+      !controversos_lgpd_pedido & controversos_lgpd_resposta ~ "Pedido não menciona LGPD, resposta menciona",
+      TRUE ~ "Não menciona LGPD"
+    )
+  )
 
-saveRDS(controversos_lgpd, here("dados/load/rds/controversos_lgpd.rds"))
+# recursos_controversos
+instancias_lvs <- c(
+  "Primeira Instância",
+  "Segunda Instância",
+  "CGU",
+  "CMRI",
+  "Pedido de Revisão"
+) %>% fct_reorder(1:6)
+
+recursos_controversos_lgpd <- recursos %>% 
+  mutate(instancia = if_else(instancia == "Terceira Instância", "CGU", instancia),
+         instancia = ordered(instancia, levels = instancias_lvs),
+         controversos_lgpd_recurso = str_detect(desc_recurso, regex_lgpd),
+         controversos_lgpd_resposta_recurso = str_detect(desc_recurso, regex_lgpd)
+         ) %>% 
+  select(id_pedido, data_resposta, instancia, orgao = orgaodestinatario, contains("controversos_lgpd")) %>%
+  mutate(
+    controverso_id = "LGPD",
+    tipo = case_when(
+      controversos_lgpd_recurso & controversos_lgpd_resposta_recurso ~ "Pedido e reposta mencionam LGPD",
+      controversos_lgpd_recurso & !controversos_lgpd_resposta_recurso ~ "Pedido menciona LGPD, resposta não",
+      !controversos_lgpd_recurso & controversos_lgpd_resposta_recurso ~ "Pedido não menciona LGPD, resposta menciona",
+      TRUE ~ "Não menciona LGPD"
+  ))
+
+saveRDS(pedidos_controversos_lgpd, here("dados/load/rds/pedidos-controversos-lgpd.rds"))
+saveRDS(recursos_controversos_lgpd, here("dados/load/rds/recursos-controversos-lgpd.rds"))
 
 # controversos_desarrazoado = str_detect(resposta_pedido,'desarrazoado|desproporciona'), 
 # controversos_fishing  = str_detect(resposta_pedido,'fishing'),
